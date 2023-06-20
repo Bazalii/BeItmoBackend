979546ebe9c4e4166f4bcd86bbeb4e079dac5d15
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 00:35:51 +0300

    feat: realise mappers for happiness

---
+++ BeItmoBackend.Web/Happiness/Mappers/HappinessWebModelsMapper.cs
@@ -0,0 +1,14 @@
+﻿using BeItmoBackend.Core.Happiness.Models;
+using BeItmoBackend.Web.Happiness.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace BeItmoBackend.Web.Happiness.Mappers;
+
+[Mapper]
+public partial class HappinessWebModelsMapper
+{
+    public partial HappinessCheckpointResponse MapHappinessCheckpointToResponse(HappinessCheckpoint checkpoint);
+
+    public partial HappinessCheckpointCreationModel MapCreationRequestToCreationModel(
+        HappinessCheckpointCreationRequest creationRequest);
+}
