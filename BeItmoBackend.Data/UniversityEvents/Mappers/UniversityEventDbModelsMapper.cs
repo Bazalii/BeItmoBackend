aaa008a69309b0b04733d24dcb2b3b5676076e13
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 00:39:19 +0300

    feat: realise mappers for university events

---
+++ BeItmoBackend.Data/UniversityEvents/Mappers/UniversityEventDbModelsMapper.cs
@@ -0,0 +1,32 @@
+﻿using BeItmoBackend.Core.Categories.Models;
+using BeItmoBackend.Core.Interests.Models;
+using BeItmoBackend.Core.UniversityEvents.Models;
+using BeItmoBackend.Data.Categories.Models;
+using BeItmoBackend.Data.Interests.Models;
+using BeItmoBackend.Data.UniversityEvents.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace BeItmoBackend.Data.UniversityEvents.Mappers;
+
+[Mapper]
+public partial class UniversityEventDbModelsMapper
+{
+    private readonly BeItmoContext _context;
+
+    public UniversityEventDbModelsMapper(BeItmoContext context)
+    {
+        _context = context;
+    }
+
+    public partial UniversityEventDbModel MapEventToDbModel(UniversityEvent universityEvent);
+    public partial UniversityEvent MapDbModelToEvent(UniversityEventDbModel dbModel);
+    public partial UniversityEventCard MapDbModelToEventCardInformation(UniversityEventDbModel dbModel);
+
+    private CategoryDbModel MapCategoryToDbModel(Category category) =>
+        _context.Categories.First(dbModel => dbModel.Id == category.Id);
+
+    private List<InterestDbModel> MapInterestsToDbModels(List<Interest> interests) =>
+        interests
+            .Select(interest => _context.Interests.First(dbModel => dbModel.Id == interest.Id))
+            .ToList();
+}
---
+++ BeItmoBackend.Web/UniversityEvents/Mappers/UniversityEventWebModelsMapper.cs
@@ -0,0 +1,28 @@
+﻿using BeItmoBackend.Core.Categories.Models;
+using BeItmoBackend.Core.Interests.Models;
+using BeItmoBackend.Core.UniversityEvents.Models;
+using BeItmoBackend.Web.UniversityEvents.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace BeItmoBackend.Web.UniversityEvents.Mappers;
+
+[Mapper]
+public partial class UniversityEventWebModelsMapper
+{
+    public partial UniversityEventCreationModel MapCreationRequestToCreationModel(
+        UniversityEventCreationRequest creationRequest);
+
+    [MapProperty(nameof(UniversityEvent.Interests), nameof(UniversityEventResponse.InterestNames))]
+    public partial UniversityEventResponse MapUniversityEventToResponse(
+        UniversityEvent universityEvent);
+
+    public partial UniversityEventCardResponse MapUniversityEventCardToResponse(
+        UniversityEventCard universityEventCard);
+
+    private string MapCategoryToName(Category category) => category.Name;
+
+    private List<string> MapInterestsToNames(List<Interest> interests) =>
+        interests
+            .Select(interest => interest.Name)
+            .ToList();
+}
