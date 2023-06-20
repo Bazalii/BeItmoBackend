176bee55878dc90d1d1636667e4fa0db66dc2d34
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-20 23:46:26 +0300

    feat: realise mappers for interests

---
+++ BeItmoBackend.Data/Interests/Mappers/InterestDbModelsMapper.cs
@@ -0,0 +1,14 @@
+﻿using BeItmoBackend.Core.Interests.Models;
+using BeItmoBackend.Data.Interests.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace BeItmoBackend.Data.Interests.Mappers;
+
+[Mapper]
+public partial class InterestDbModelsMapper
+{
+    public partial InterestDbModel MapInterestToDbModel(Interest interest);
+    public partial Interest MapDbModelToInterest(InterestDbModel dbModel);
+    public partial InterestStatisticDbModel MapInterestStatisticToDbModel(InterestStatistic statistic);
+    public partial InterestStatistic MapDbModelToInterestStatistic(InterestStatisticDbModel statistic);
+}
