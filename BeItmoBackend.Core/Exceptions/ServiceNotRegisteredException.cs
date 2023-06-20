045892bade896c4d53bef0a9b630d1068d9afbe3
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 00:42:46 +0300

    feat: realise two exceptions
    - Realise ObjectNotFoundException and ServiceNotRegisteredException

---
+++ BeItmoBackend.Core/Exceptions/ServiceNotRegisteredException.cs
@@ -0,0 +1,9 @@
+﻿namespace BeItmoBackend.Core.Exceptions;
+
+public class ServiceNotRegisteredException : Exception
+{
+    public ServiceNotRegisteredException(string message)
+        : base(message)
+    {
+    }
+}
