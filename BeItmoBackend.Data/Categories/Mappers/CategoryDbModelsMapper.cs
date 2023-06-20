373bb663db63db2663b6a4af193fe28056ad125e
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-20 23:30:49 +0300

    feat: realise mappers for categories

---
+++ BeItmoBackend.Data/Categories/Mappers/CategoryDbModelsMapper.cs
@@ -0,0 +1,12 @@
+﻿using BeItmoBackend.Core.Categories.Models;
+using BeItmoBackend.Data.Categories.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace BeItmoBackend.Data.Categories.Mappers;
+
+[Mapper]
+public partial class CategoryDbModelsMapper
+{
+    public partial CategoryDbModel MapCategoryToDbModel(Category category);
+    public partial Category MapDbModelToCategory(CategoryDbModel dbModel);
+}
