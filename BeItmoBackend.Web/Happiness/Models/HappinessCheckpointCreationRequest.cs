9c76902bc50dee5120c4552ce11d6f8e4a3e679e
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 00:35:12 +0300

    feat: realise models for happiness

---
+++ BeItmoBackend.Web/Happiness/Models/HappinessCheckpointCreationRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace BeItmoBackend.Web.Happiness.Models;
+
+public class HappinessCheckpointCreationRequest
+{
+    public int UserId { get; set; }
+    public string Note { get; set; } = string.Empty;
+    public int Score { get; set; }
+}
---
+++ BeItmoBackend.Web/Happiness/Models/HappinessCheckpointResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace BeItmoBackend.Web.Happiness.Models;
+
+public class HappinessCheckpointResponse
+{
+    public Guid Id { get; set; }
+    public int UserId { get; set; }
+    public DateOnly Date { get; set; }
+    public string Note { get; set; } = string.Empty;
+    public int Score { get; set; }
+}
