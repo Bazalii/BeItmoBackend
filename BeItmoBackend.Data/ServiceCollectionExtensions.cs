e16d4b8cb088cf1aee086af4e2aa9c82d6be2e3d
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 18:10:25 +0300

    feat: realise IServiceCollection extensions for all projects

---
+++ BeItmoBackend.Data/ServiceCollectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using BeItmoBackend.Core.Categories.Repositories;
+using BeItmoBackend.Core.CommonClasses;
+using BeItmoBackend.Core.Happiness.Repositories;
+using BeItmoBackend.Core.Interests.Repositories;
+using BeItmoBackend.Core.UniversityEvents.Repositories;
+using BeItmoBackend.Core.Users.Repositories;
+using BeItmoBackend.Data.Categories.Mappers;
+using BeItmoBackend.Data.Categories.Repositories;
+using BeItmoBackend.Data.Happiness.Mappers;
+using BeItmoBackend.Data.Happiness.Repositories;
+using BeItmoBackend.Data.Interests.Mappers;
+using BeItmoBackend.Data.Interests.Repositories;
+using BeItmoBackend.Data.UniversityEvents.Mappers;
+using BeItmoBackend.Data.UniversityEvents.Repositories;
+using BeItmoBackend.Data.Users.Mappers;
+using BeItmoBackend.Data.Users.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeItmoBackend.Data;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddData(this IServiceCollection serviceCollection, IConfiguration configuration)
+    {
+        serviceCollection.AddDbContext<BeItmoContext>(
+            options => options
+                .UseLazyLoadingProxies()
+                .UseNpgsql(configuration["DbConnectionString"]));
+
+        serviceCollection.AddScoped<ICategoryRepository, CategoryRepository>();
+        serviceCollection.AddScoped<IHappinessRepository, HappinessRepository>();
+        serviceCollection.AddScoped<IInterestRepository, InterestRepository>();
+        serviceCollection.AddScoped<IUniversityEventRepository, UniversityEventRepository>();
+        serviceCollection.AddScoped<IUserRepository, UserRepository>();
+
+        serviceCollection.AddScoped<CategoryDbModelsMapper>();
+        serviceCollection.AddScoped<HappinessDbModelsMapper>();
+        serviceCollection.AddScoped<InterestDbModelsMapper>();
+        serviceCollection.AddScoped<UniversityEventDbModelsMapper>();
+        serviceCollection.AddScoped<UserDbModelsMapper>();
+
+        serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return serviceCollection;
+    }
+}
---
+++ BeItmoBackend.Web/ServiceCollectionExtensions.cs
@@ -0,0 +1,33 @@
+﻿using BeItmoBackend.Web.Categories.Mappers;
+using BeItmoBackend.Web.Happiness.Mappers;
+using BeItmoBackend.Web.HostedServices;
+using BeItmoBackend.Web.Interests.Mappers;
+using BeItmoBackend.Web.UniversityEvents.Mappers;
+using BeItmoBackend.Web.Users.Mappers;
+using Microsoft.OpenApi.Models;
+
+namespace BeItmoBackend.Web;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddWeb(this IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddScoped<CategoryWebModelsMapper>();
+        serviceCollection.AddScoped<HappinessWebModelsMapper>();
+        serviceCollection.AddScoped<InterestWebModelsMapper>();
+        serviceCollection.AddScoped<UniversityEventWebModelsMapper>();
+        serviceCollection.AddScoped<UserWebModelsMapper>();
+
+        serviceCollection.AddHostedService<MigrationHostedService>();
+
+        serviceCollection.AddControllers();
+
+        serviceCollection.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc(
+                "v1", new OpenApiInfo { Title = "BeItmoBackend", Version = "v1" });
+        });
+
+        return serviceCollection;
+    }
+}
