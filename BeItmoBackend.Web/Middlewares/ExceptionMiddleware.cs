821365557378f287d262014952485d3717499c74
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 18:09:33 +0300

    feat: realise ExceptionMiddleware

---
+++ BeItmoBackend.Web/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using BeItmoBackend.Core.Exceptions;
+
+namespace BeItmoBackend.Web.Middlewares;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (ObjectNotFoundException objectNotFoundException)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status404NotFound;
+            await httpContext.Response.WriteAsJsonAsync(new { Error = $"{objectNotFoundException.Message}" });
+        }
+        catch (NotAuthorizedException notAuthorizedException)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            await httpContext.Response.WriteAsJsonAsync(new { Error = $"{notAuthorizedException.Message}" });
+        }
+        catch (Exception exception)
+        {
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await httpContext.Response.WriteAsJsonAsync(new { Error = $"{exception.Message}" });
+        }
+    }
+}
