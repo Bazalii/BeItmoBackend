8835e8204213f78c58ecfc6c5cb0ee5162e702f8
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 18:09:54 +0300

    feat: realise AuthenticationMiddleware

---
+++ BeItmoBackend.Web/Middlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using BeItmoBackend.Core.Exceptions;
+
+namespace BeItmoBackend.Web.Middlewares;
+
+public class AuthenticationMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public AuthenticationMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext httpContext)
+    {
+        var token = httpContext.Request.Headers.Authorization.ToString();
+
+        if (token.Length == 0)
+        {
+            throw new NotAuthorizedException("You are not authorized!");
+        }
+
+        var handler = new JwtSecurityTokenHandler();
+        var jwtSecurityToken = handler.ReadJwtToken(token);
+
+        var isuNumber = jwtSecurityToken.Claims.FirstOrDefault(claim => claim.Type == "isu") ??
+                        throw new NotAuthorizedException("You are not authorized!");
+
+        httpContext.Items["isuNumber"] = int.Parse(isuNumber.Value);
+
+        await _next(httpContext);
+    }
+}
