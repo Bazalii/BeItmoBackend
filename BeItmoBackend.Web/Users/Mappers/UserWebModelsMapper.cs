3c5b2534f22e3bdbfc8c58f617bcbbf5ed981f51
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 14:49:29 +0300

    feat: realise mappers for users

---
+++ BeItmoBackend.Web/Users/Mappers/UserWebModelsMapper.cs
@@ -0,0 +1,11 @@
+﻿using BeItmoBackend.Core.Users.Models;
+using BeItmoBackend.Web.Users.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace BeItmoBackend.Web.Users.Mappers;
+
+[Mapper]
+public partial class UserWebModelsMapper
+{
+    public partial UserResponse MapUserToUserResponse(User user);
+}
