0c67f79c476176869992e19eede06b878bd18bef
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-21 23:50:24 +0300

    feat: realise UserStatisticsRepository and mapper for user statistic models

---
+++ BeItmoBackend.Core/UserAnalytics/UserStatistics/Repositories/IUserStatisticsRepository.cs
@@ -0,0 +1,26 @@
+﻿using BeItmoBackend.Core.UserAnalytics.UserStatistics.Enums;
+using BeItmoBackend.Core.UserAnalytics.UserStatistics.Models;
+
+namespace BeItmoBackend.Core.UserAnalytics.UserStatistics.Repositories;
+
+public interface IUserStatisticsRepository
+{
+    Task<UserStatistic> AddAsync(UserStatistic statistic, CancellationToken cancellationToken);
+
+    Task<UserStatistic> GetAsync(Guid typeValueId, int userId, StatisticType type,
+                                 CancellationToken cancellationToken);
+
+    Task<List<UserStatistic>> GetAllForUserAndTypeAsync(int userId, StatisticType type,
+                                                        CancellationToken cancellationToken);
+
+    Task<bool> ExistsAsync(Guid typeValueId, int userId, StatisticType type, CancellationToken cancellationToken);
+
+    Task IncrementTapCounterAsync(Guid typeValueId, int userId, StatisticType type,
+                                  CancellationToken cancellationToken);
+
+    Task IncrementPrizeCounterAsync(Guid typeValueId, int userId, StatisticType type,
+                                    CancellationToken cancellationToken);
+
+    Task RemoveAsync(Guid typeValueId, int userId, StatisticType type,
+                     CancellationToken cancellationToken);
+}
