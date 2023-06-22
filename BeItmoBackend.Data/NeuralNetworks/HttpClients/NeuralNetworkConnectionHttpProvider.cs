16c0d81cad1e126abc897efd458dde3ff5edc911
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-22 11:10:16 +0300

    feat: add environment variable for neural network service host
    - Add usage of variable to NeuralNetworkConnectionHttpProvider

--- BeItmoBackend.Data/NeuralNetworks/HttpClients/NeuralNetworkConnectionHttpProvider.cs
+++ BeItmoBackend.Data/NeuralNetworks/HttpClients/NeuralNetworkConnectionHttpProvider.cs
@@ -4,24 +4,25 @@ using System.Text.Json;
 using BeItmoBackend.Core.Exceptions;
 using BeItmoBackend.Core.NeuralNetworks.Repositories;
 using BeItmoBackend.Data.NeuralNetworks.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace BeItmoBackend.Data.NeuralNetworks.HttpClients;
 
 public class NeuralNetworkConnectionHttpProvider : INeuralNetworkConnectionProvider
 {
-    //TODO get BasePath from configuration
-    private const string BasePath = "http://localhost:8000/";
+    private readonly string _host;
     private readonly HttpClient _httpClient;
 
-    public NeuralNetworkConnectionHttpProvider(HttpClient httpClient)
+    public NeuralNetworkConnectionHttpProvider(HttpClient httpClient, IConfiguration configuration)
     {
         _httpClient = httpClient;
+        _host = configuration["NeuralNetworkServiceHost"]!;
     }
 
     public async Task<int> GetEmotionsStatus(string message, CancellationToken cancellationToken)
     {
         var request = new HttpRequestMessage(HttpMethod.Post,
-                                             $"{BasePath}understandingEmotions/predictEmotion/");
+                                             $"{_host}understandingEmotions/predictEmotion/");
         request.Content =
             new StringContent(JsonSerializer.Serialize(new { message }), Encoding.UTF8, "application/json");
 
